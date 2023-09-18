b68398605985568f3622c3f1bc9ab151dcd96eeb
azrarafeeq <azraaminah@gmail.com>
2023-09-18 14:11:57 +0400

    chat module potential endpoints&services  before injecting repo

--- backend/src/chat/chat.controller.ts
+++ backend/src/chat/chat.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, UseGuards, Req, Res, Post, HttpCode, Body, UnauthorizedException } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req, Res, Post, HttpCode, Body, UnauthorizedException, Param } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { ChatService } from './chat.service';
 
 @Controller('chat')
 export class ChatController {
-	constructor(private JwtService: JwtService){
+	constructor(private JwtService: JwtService, private chatService: ChatService){
 	}
 	@Get('channels')
 	async all_channels(){
@@ -18,52 +19,81 @@ export class ChatController {
 	}
 
 	@Post('/:chan_name')
-	async create_chan(){
-		
+	async create_chan(@Param('chan_name') chan_name: string){
+		if (this.chatService.chan_exist(chan_name))
+			this.chatService.add_chan(chan_name);
+		// else
+			// error: chan already exist
 	}
 	@Post('/:chan_name/join')
-	async joinChan(){
-
+	async joinChan(@Req() req, @Param('chan_name') chan_name: string){
+		if (this.chatService.chan_exist(chan_name))
+			this.chatService.add_chan_mem(req.user); // pass User as param or user details individually
+		// else  (maybe not necessary)
+			// throw channel does not exist error or create channel??
 	}
 
 	@Post('/:chan_name/leave')
-	async leaveChan(){
-
+	async leaveChan(@Req() req, @Param('chan_name') chan_name: string){
+		if (this.chatService.is_chan_mem(req.user.userName, chan_name))
+			this.chatService.rm_chan_mem(req.user.userName, chan_name);
+		// else (maybe not necessary)
+			// not member of channel
 	}
 
 	@Post('/:chan_name/message')
-	async sendMsg(){
+	async sendChanMsg(@Req() req, @Param('chan_name')chan_name: string){
+		// after check
+		this.chatService.msg_chan(req.user.userName, chan_name)
+	}
+
+	@Post('/:chan_name/invite') // the user to be invited also
+	async inviteUser(@Req() req, @Param('chan_name') chan_name: string){
 
 	}
 
-	@Post('/:chan_name/invite')
-	async inviteUser(){
-
-	}
-
-	@Post('/:chan_name/set_pass')
-	async set_pass(){
-
+	@Post('/:chan_name/set_pass') // password
+	async set_pass(@Req() req, @Param('chan_name') chan_name: string){
+		// if (this.chatService.is_owner(req.user.userName, chan_name))
+			// this.chatService.set_pass(chan_name, /* password */)
+		// else
+			// error: user not an owner (unauthorized)
 	}
 
 	@Post('/:chan_name/set_admin')
-	async set_admin(){
-
+	async set_admin(@Req() req, @Param('chan_name') chan_name: string){
+		// if (this.chatService.is_owner(req.user.userName, chan_name))
+			// this.chatService.set_admin(chan_name, /* user to be set as admin */);
+		// else
+			// error: user not an owner (unauthorized)
 	}
 
 	@Post('/:chan_name/kick/:id')
-	async kick_user(){
+	async kick_user(@Req() req, @Param('chan_name') chan_name: string){
+		// if (this.chatService.is_owner(req.user.userName, chan_name) || 
+			// this.chatService.is_admin(req.user.userName, chan_name))
+			// this.chatService.kick_user(/* user to kick */, chan_name);
+		// else
+			// error: user not an owner or an admin (unauthorized)
 
 	}
 
 	@Post('/:chan_name/ban/:id')
-	async ban_user(){
-
+	async ban_user(@Req() req, @Param('chan_name') chan_name: string){
+		// if (this.chatService.is_owner(req.user.userName, chan_name) || 
+			// this.chatService.is_admin(req.user.userName, chan_name))
+			// this.chatService.ban_user(/* user to ban */, chan_name);
+		// else
+			// error: user not an owner or an admin (unauthorized)
 	}
 
 	@Post('/:chan_name/mute/:id')
-	async mute_user(){
-
+	async mute_user(@Req() req, @Param('chan_name') chan_name: string){
+		// if (this.chatService.is_owner(req.user.userName, chan_name) || 
+			// this.chatService.is_admin(req.user.userName, chan_name))
+			// this.chatService.mute_user(/* user to mute */, chan_name);
+		// else
+			// error: user not an owner or an admin (unauthorized)
 	}
 
 }
--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -1,40 +1,120 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../users/user.entity';
+import { channel } from 'diagnostics_channel';
+import * as request from 'supertest';
 
 @Injectable()
 export class ChatService {
 	 // -  have to inject database/repo -
+	 constructor(/* Inject repo */){}
 
 	// view all channels
+	async get_all_chan(){
+			// return <Array>? of all channels from repo
+		}
 
 	// view all channel members
+	async mem_by_chan(chan_name: string){
+		// retrun <Array>? of memebers of the given channel
+	}
 
 	// get channel by name
-
+	async chan_exist(chan_name: string)
+	{
+		// chan = <repo>.findOneBy({chan_name});
+		// if (chan)
+		// 	return true;
+		// else
+		// 	return false;
+	}
 	// join channel
+	async add_chan_mem(user: User)
+	{
+		// insert or create the user in the channel memeber table
+	}
 
 	// leave channel
+	async rm_chan_mem(userName: string, chan_name: string)
+	{
+		// const user = await this.<repo>.findOne({userName});
+		// if (user)
+		// return (this.<repo>.delete(userName))
+	}
 
 	// message channel
+	msg_chan(sender: string, chan_name: string)
+	{
+
+	}
 
 	// message individual user
+	msg_user(sender: string, reciever: string)
+	{
+		
+	}
 
 	// create channel
+	async add_chan(chan_name: string)
+	{
+		// this.<repo>.create (chan_name: string); // and other specifications if any
+	}
 
 	// if_admin
-
+	async is_admin(user_name: string, chan_name: string)
+	{
+		// user = // find user from repo admin table
+		// if (user)
+		// 	return true;
+		// else
+		// 	return false;
+	}
 	// if_owner
+	async is_owner(user_name: string, chan_name: string)
+	{
+		// user = // find user from repo owner table
+		// if (user)
+		// 	return true;
+		// else
+		// 	return false;
+	}
+	async is_chan_mem(user_name: string, chan_name: string)
+	{
+		// user = this.repo.findOneBy({user_name});
+		// if (user)
+		// 	return true
+		// else
+		// 	return false;
+	}
 
 	// SPECIFIC TO OWNERS
 	// set channel password
+	async set_pass(chan_name: string, pass: string)
+	{
 
+			// change password of the channel (from repo or local storage?)
+		
+	}
 	// set admin
+	async set_admin(req_user: string, chan_name: string, user_name: string)
+	{
+			// update user as admin 
+	}
 
 	// SPECIFIC TO OWNERS/ADMINISTRATORS
 	// kick user
-
+	async kick_user(user_to_kick: string, chan_name: string)
+	{
+			// rm chan_mem
+	}
 	// ban user
-
+	async ban_user(user_to_ban: string, chan_name: string)
+	{
+			// add user to the banned table of the 
+	}
 	// mute user
-
-
+	async mute_user(req_user: string, user_to_mute: string, chan_name: string)
+	{
+		// if (this.is_owner(req_user) || this.is_admin(req_user))
+			// add user to the mute table??
+	}
 }
