5d0619741a8f0242f49cf0d9f3d8688f856ed5af
o7ab <omaremad951@gmail.com>
2023-10-29 18:31:04 +0400

    made a dockerfile for the backend

--- backend/src/ormconfig.ts
+++ backend/src/ormconfig.ts
@@ -1,9 +1,8 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-// import { Channel } from "./entities/channel.entity";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
 	type: 'postgres',
-	host: 'localhost',
+	host: 'database',
 	port: 5432,
 	username: 'postgres',
 	password: '123',
