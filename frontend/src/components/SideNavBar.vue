754442aa5059548b878edc8eea5788678e40ff23
Abdullah Ghazi <aghazi@lab1r3s12.42abudhabi.ae>
2023-11-01 12:54:57 +0400

    fixed login page and add WebSocket

--- frontend/src/components/SideNavBar.vue
+++ frontend/src/components/SideNavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <aside class="bg-gradient-to-t from-[#AE445A] to-[#451952] shadow-custom  p-5 min-h-[85.4vh] md:min-h-[89.50vh] lg:min-h-[98.50vh]">
+  <aside class="bg-gradient-to-t from-[#AE445A] to-[#451952] shadow-custom  p-5 min-h-[85.4vh] md:min-h-[85.10vh] lg:min-h-[85.9vh]">
     <ul class="list-none p-0">
       <li class="mb-5">
         <router-link to="/chat" 
